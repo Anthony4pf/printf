1f79f4d6cdb2f5ee5f81c5f09183ebbe5dbf183e
imrana abdulkadir <imranaabdulkadir6@gmail.com>
2022-11-21 14:11:16 -0800

    awesome

---
+++ print_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_string - print string to stdout
+*@garg: argument
+*Return: printed string
+*/
+
+int_print string(va_list arg)
+{
+       char *str;
+       unsigned int 1;
+
+       str = vs_arg(arg, char *);
+
+       for (1 = 0; str(1); 1++)
+       {
+              _putchar(str(1));
+       }
+
+       return (1);
+}
